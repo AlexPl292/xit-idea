efe55b8f9ca7eb0323e12173b9a76f568352a850
GitHub Action <action@github.com>
2022-08-24 05:02:44 +0000

    Template cleanup

--- src/main/kotlin/com/github/alexpl292/xitidea/MyBundle.kt
+++ src/main/kotlin/com/github/alexpl292/xitidea/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.alexpl292.xitidea
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/alexpl292/xitidea/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/alexpl292/xitidea/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.alexpl292.xitidea.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.alexpl292.xitidea.MyBundle
 
 class MyApplicationService {
 
